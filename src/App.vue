0df2ee86f5561d9231159ff3dc428d2925a7f1bc
Loong <liuxiaolong_xj@126.com>
2020-03-19 23:13:47 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    初步学习{{title}}
+  </div>
+</template>
+<script>
+// export default {
+//     steup () {
+//       title: 'vue3.0'
+//     }
+//   }
+
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup () {
+    return {
+      title: 'vue3.0'
+    }
+  }
+})
+</script>
+<style scope>
+div {
+  width: 100px;
+  height: 100px;
+  background: #ccc;
+  color: skyblue;
+  margin: 20px;
+  text-align: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import {
+    createApp
+} from 'vue'
+import App from './App.vue'
+
+// 创建对应对象
+const app = createApp(App)
+
+app.mount('#app')
